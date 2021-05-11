6041006b5c8e341921f9963f1e815bf3423e6075
legik777 <75944488+legik777@users.noreply.github.com>
2021-05-11 12:27:34 +0300

    Update menu.hpp

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,28 +1,32 @@
 // Copyright 2020 Andreytorix
-#pragma once
-#include <boost/program_options.hpp>
-#include <string>
-class EApplication {
-	char** argv;
-	int argc;
-public:
+#pragma once#include <boost/program_options.hpp>
 
-	void menu(int argc, const char** argv);
-	int exec();
-	std::string path_to_programm;
-private:
-	boost::program_options::positional_options_description p;
-	
-	boost::program_options::options_description m_desk{ "äîñòóïíûå îïöèè" };
-	boost::program_options::variables_map m_vm;
-	/*
-	--log-level
-	--thread-count
-	--output
-	*/
-	std::string log_level;      //
-	int thread_count;   //êîëè÷åñòâî ïîòîêîâ
-	std::string path;     //ïóòü ê ôàéëó ñ ðåçóëüòàòîì
-	std::string input_path; 
-	
+#include <string>
+
+class EApplication {
+    char ** argv;
+    int argc;
+    public:
+
+        void menu(int argc,
+            const char ** argv);
+    int exec();
+    std::string path_to_programm;
+    private:
+        boost::program_options::positional_options_description p;
+
+    boost::program_options::options_description m_desk {
+        "äîñòóïíûå îïöèè"
+    };
+    boost::program_options::variables_map m_vm;
+    /*
+    --log-level
+    --thread-count
+    --output
+    */
+    std::string log_level; //
+    int thread_count; //êîëè÷åñòâî ïîòîêîâ
+    std::string path; //ïóòü ê ôàéëó ñ ðåçóëüòàòîì
+    std::string input_path;
+
 };
