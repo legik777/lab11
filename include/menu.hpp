deabdd0f2a962af272c454694bc8b915588162d0
legik777 <75944488+legik777@users.noreply.github.com>
2021-05-24 22:57:43 +0300

    Update menu.hpp

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,33 +1,22 @@
 // Copyright 2020 Andreytorix
 #pragma once
 #include <boost/program_options.hpp>
-
 #include <string>
-
 class EApplication {
-    //char ** argv;
-    //int argc;
-
+	char** argv1;
+	int argc1;
 public:
-        void menu(int argc,
-            const char ** argv);
-    int exec();
-    std::string path_to_programm;
 
+	void menu(int argc, const char** argv);
+	int exec();
+	std::string path_to_programm;
 private:
-        boost::program_options::positional_options_description p;
+	boost::program_options::positional_options_description p;
 
-    boost::program_options::options_description m_desk {
-        "äîñòóïíûå îïöèè"
-    };
-    boost::program_options::variables_map m_vm;
-    /*
-    --log-level
-    --thread-count
-    --output
-    */
-    std::string log_level; //
-    int thread_count; //êîëè÷åñòâî ïîòîêîâ
-    std::string path; //ïóòü ê ôàéëó ñ ðåçóëüòàòîì
-    std::string input_path;
+	boost::program_options::options_description m_desk{ "доступные опции" };
+	boost::program_options::variables_map m_vm;
+	std::string conf;      
+	bool insta;
+	bool pack;
+	int timeout;
 };
