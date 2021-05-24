9539ed6192ecad781ba7c2393081914e20c911f9
legik777 <75944488+legik777@users.noreply.github.com>
2021-05-24 23:03:06 +0300

    Update menu.hpp

--- include/menu.hpp
+++ include/menu.hpp
@@ -3,20 +3,18 @@
 #include <boost/program_options.hpp>
 #include <string>
 class EApplication {
-	char** argv1;
-	int argc1;
+    char** argv1;
+    int argc1;
 public:
-
-	void menu(int argc, const char** argv);
-	int exec();
-	std::string path_to_programm;
+    void menu(int argc, const char** argv);
+    int exec();
+    std::string path_to_programm;
 private:
-	boost::program_options::positional_options_description p;
-
-	boost::program_options::options_description m_desk{ "доступные опции" };
-	boost::program_options::variables_map m_vm;
-	std::string conf;      
-	bool insta;
-	bool pack;
-	int timeout;
+    boost::program_options::positional_options_description p;
+    boost::program_options::options_description m_desk{ "доступные опции" };
+    boost::program_options::variables_map m_vm;
+    std::string conf;      
+    bool insta;
+    bool pack;
+    int timeout;
 };
