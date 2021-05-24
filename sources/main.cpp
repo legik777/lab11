a8519aded78278d7549711a86a96fc6c4dea6466
legik777 <75944488+legik777@users.noreply.github.com>
2021-05-24 22:56:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,14 @@
 // Copyright 2020 Andreytorix
+#include <async++.h>
 #include <iostream>
-#include "../include/menu.hpp"
-int main(int argc, const char* argv[])
-{
+#include <memory>
+#include <boost/process.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
+#include <boost/iostreams/stream.hpp>
+#include "menu.hpp"
+int main(int argc, const char *argv[]) {
     setlocale(LC_ALL, "Russian");
     EApplication obj;
     obj.menu(argc, argv);
